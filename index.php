e2cf7baf97319b8748c85e9b34312a1b8acbe808
Lisa Gay <lisagay@alumni.uchicago.edu>
2016-09-17 19:08:18 -0400

    Added form and first sketches of validation and CSS

--- index.php
+++ index.php
@@ -8,13 +8,22 @@ ini_set('display_errors', 1); # Display errors on page (instead of a log file)
 <html>
 <head>
   <title>Password Generator</title>
-  <meta charset="utf-8" />
-  <meta name="description" content="A simple xkcd-style password generator" />
+  <meta charset='utf-8' />
+  <meta name='description' content='A simple xkcd-style password generator' />
   <?php require('logic.php'); ?>
-  <!-- ADD STYLESHEET WHEN READY <link rel="stylesheet" href="styles.css"> -->
+  <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-  <h1>Hey! Here, have a password!</h1>
-  <p><?php echo($password); ?></p>
+  <h1>Create a Password</h1>
+  <form action='index.php' method='post'>
+    Number of words: <input type='number' name='wordCount' min='1' max='10'><br/>
+    <fieldset>
+      <legend>Seperator:</legend>
+      <input type='radio' name='joiningChar' id='hyphen' value='-' checked='checked'> Hyphen<br/>
+      <input type='radio' name='joiningChar' id='underscore' value='_'> Underscore<br/>
+    </fieldset>
+    <input type='submit' value='Get Password' max='10' min='1'>
+  </form>
+  <p><?php echo($displayMessage); ?></p>
 </body>
 </html>
--- logic.php
+++ logic.php
@@ -1,22 +1,74 @@
 <?php
 
+  /* Validation to come:
+  If $wordCount is_integer is false
+  If $wordCount < 1
+  If $wordCount > 10
+  Then highlight field in red and display message:
+  Enter an integer between 1 and 10 */
+
   /* Define variables:
   words to build passwords (array),
   counter for number of words in passwords,
   the character to put between password elements,
-  placeholder varaible that will hold the password elements */
+  placeholder varaible that will hold the password elements,
+  message for server-side validation,
+  the text that will actually be displayed (password or message) */
   $wordsArray = ['raven', 'writing', 'desk', 'gyre', 'gimble', 'caterpiller', 'little', 'bat', 'bandersnatch', 'fruminous', 'diane', 'vorpal', 'snicker', 'snack', 'brillig'];
-  $wordCount = 4;
-  $joiningChar = '-';
+  $wordCount = '';
+  $joiningChar = '';
   $password = '';
+  $validationMessage = '';
+  $displayMessage = '';
+
+  /* Validation message */
+  if ($_POST['wordCount'] < 2) {
+    $validationMessage = 'Too small! Just one word isn\'t secure at all.';
+  } elseif ($_POST['wordCount'] > 9) {
+    $validationMessage = 'Too big! Longer is more secure, but will you really be able to remember more than nine words?';
+  }
+  //elseif (is_integer($_POST['wordCount']) === false) {
+//    $validationMessage = 'Integers only, please.';
+//  } 
+  else {
+    $validationMessage = 'OK';
+  }
+
+//    elseif ($_POST['wordCount'] > 9) {
+//      $validationMessage = 'T'
+//    } elseif (is_int($_POST['wordCount'])==false) {
+//      $validationMessage = 'Integers only, please.'
+//    }
+//  }
 
   /* Randomize the order of words in the array */
 
   shuffle($wordsArray);
 
-  $password = $wordsArray[0] . $joiningChar . $wordsArray[1];
+  /* Get the number of words and the joining character the user wants */
 
+  $wordCount = $_POST['wordCount']-1;
+  $joiningChar = $_POST['joiningChar'];
 
+  /* Select the specified number of elements from the array and concatenate */
+
+  for ($i = $wordCount; $i >= 0; $i--) {
+    $password = $password . $wordsArray[$i];
+
+    /* If it's not the last element, add the joining character */
+
+    if ($i > 0) {
+      $password = $password . $joiningChar;
+    }
+  }
+
+  /* If the validation passes, show the password. Otherwise, show the validation message. */
+
+  if ($validationMessage == 'OK') {
+    $displayMessage = $password;
+  } else {
+    $displayMessage = $validationMessage;
+  }
 
 
 ?>
