8a48c7e40b9af572e7bfd601ce6a25b0a47808fd
Lisa Gay <lisagay@alumni.uchicago.edu>
2016-09-22 19:58:51 -0400

    Fixing bug in validation (for real this time)

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ Features include an input box to specify the number of words to concatenate, a c
 
 To see the server-side validation messages, try a word a count of 10 or 1. Use a code inspector to change the input type to text to trip the `is_numeric()` condition.
 
+UPDATE SEPT. 22: In my demo, I pointed out that I didn't notice a major flaw in the validation logic until very late and wasn't able to fix that before submitting the completed project. With Susan's permission, I have fixed that bug after submitting my assignment, with assistance from Dan. Thanks, Dan!
+
 ## Citations
 
 I referred to the following resources:
--- logic.php
+++ logic.php
@@ -22,9 +22,10 @@ $messageClass = 'error';
 Check whether the user has submitted any values yet.
 If not, prompt user to enter the number of words.
 */
-if (isset($_GET['wordCount']) && isset($_GET['joiningChar'])) {
+
+if (isset($_GET['wordCount']) || isset($_GET['joiningChar'])) {
     /*
-    Validation: Show an error if the number entered is not 2-9
+    Validation: Show an error if the value entered is not 2-9
     */
     if (empty($_GET['wordCount'])) {
         $validationMessage = 'Enter a number of words to include.';
@@ -38,67 +39,69 @@ if (isset($_GET['wordCount']) && isset($_GET['joiningChar'])) {
         $validationMessage = 'OK';
         $messageClass = 'password';
     }
-    /*
-    Randomize the order of words in the array
-    */
-    shuffle($wordsArray);
-    /*
-    Put the user's values into the variables
-    */
-    $wordCount = $_GET['wordCount']-1;
-    $joiningChar = $_GET['joiningChar'];
-    if (!empty($_GET['addNumber'])) {
-        array_push($options,$_GET['addNumber']);
-    }
-    if (!empty($_GET['addSymbol'])) {
-        array_push($options,$_GET['addSymbol']);
-    }
-    /*
-    Concatenate the specified number of words with the separator
-    */
-    for ($i = $wordCount; $i >= 0; $i--) {
-        $password = $password . $wordsArray[$i];
-        /*
-        If it's not the last element, add the separator
-        */
-        if ($i > 0) {
-            $password = $password . $joiningChar;
-        }
-    }
-    /*
-    If the user chose to add a number,
-    pick a random digit (0-9) and add to the end of the password
-    */
-    if (in_array('number',$options)) {
-        $password = $password . $joiningChar . rand(0,9);
-    }
-    /*
-    If the user chose to add a symbol,
-    pick a "chunk" of the ASCII series that contains only symbols
-    (there are four such chunks), then pick a random number in that chunk
-    and add to the end of the password
-    */
-    if (in_array('symbol',$options)) {
-        $symbolSet = rand(1,4);
-        if ($symbolSet == 1) {
-            $symbolCode = rand(33,47);
-        } elseif ($symbolSet == 2) {
-            $symbolCode = rand(58,64);
-        } elseif ($symbolSet == 3) {
-            $symbolCode = rand(91,96);
-        } else {
-            $symbolCode = rand(123,126);
-        }
-        $password = $password . $joiningChar . chr($symbolCode);
-    }
-    /*
-    If the validation passes, show the password.
-    If not, show the validation message.
-    */
-    if ($validationMessage == 'OK') {
-        $displayMessage = $password;
-    } else {
+
+    if ($validationMessage != 'OK') {
         $displayMessage = $validationMessage;
+    } else {
+        /*
+        Randomize the order of words in the array
+        */
+        shuffle($wordsArray);
+        /*
+        Put the user's values into the variables
+        */
+        $wordCount = $_GET['wordCount']-1;
+        $joiningChar = $_GET['joiningChar'];
+        if (!empty($_GET['addNumber'])) {
+            array_push($options,$_GET['addNumber']);
+        }
+        if (!empty($_GET['addSymbol'])) {
+            array_push($options,$_GET['addSymbol']);
+        }
+        /*
+        Concatenate the specified number of words with the separator
+        */
+        for ($i = $wordCount; $i >= 0; $i--) {
+            $password = $password . $wordsArray[$i];
+            /*
+            If it's not the last element, add the separator
+            */
+            if ($i > 0) {
+                $password = $password . $joiningChar;
+            }
+        }
+        /*
+        If the user chose to add a number,
+        pick a random digit (0-9) and add to the end of the password
+        */
+        if (in_array('number',$options)) {
+            $password = $password . $joiningChar . rand(0,9);
+        }
+        /*
+        If the user chose to add a symbol,
+        pick a "chunk" of the ASCII series that contains only symbols
+        (there are four such chunks), then pick a random number in that chunk
+        and add to the end of the password
+        */
+        if (in_array('symbol',$options)) {
+            $symbolSet = rand(1,4);
+            if ($symbolSet == 1) {
+                $symbolCode = rand(33,47);
+            } elseif ($symbolSet == 2) {
+                $symbolCode = rand(58,64);
+            } elseif ($symbolSet == 3) {
+                $symbolCode = rand(91,96);
+            } else {
+                $symbolCode = rand(123,126);
+            }
+            $password = $password . $joiningChar . chr($symbolCode);
+        }
+        /*
+        If the validation passes, show the password.
+        If not, show the validation message.
+        */
+
+        $displayMessage = $password;
     }
 } else {
     $displayMessage = 'Enter a number of words to include.';
